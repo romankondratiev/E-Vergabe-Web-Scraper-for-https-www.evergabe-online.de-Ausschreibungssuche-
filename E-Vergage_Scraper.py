295cae5a8c88dab0cb37e269733929888e969540
Roman <romhedoo@gmail.com>
2018-06-09 16:44:30 +0200

    updated

---
+++ E-Vergage_Scraper.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup as BS
+import requests 
+import pandas as pd
+import os
+
+keywords = ['Umfrage', 'Studie', 'Befragung']
+prefix = 'https://www.evergabe-online.de/search.html?searchString='
+ending = '&publishDateRange=ALL'
+prefix2 = "https://www.evergabe-online.de"
+
+description = []
+scraperlinks = []
+releasedates = []
+deadlinedates = []
+hyperlinks = []
+i=0
+
+print("Suche auf ",prefix2, "nach Begriff:")
+for keyword in keywords:
+	i=i+1
+	print(" ", i, ": ", keyword, sep="")
+	text = requests.get(prefix + keyword + ending).text
+	soup = BS(text, "lxml")
+	descr_scraperlinks = soup.find_all("tr", {"class": ["odd", "even"]})
+	for element in descr_scraperlinks:
+		description.append(element.a.get_text()) 
+		scraperlinks.append(prefix2 + element.a["href"])
+		hyperlinks.append('=hyperlink(' '"' + prefix2 + element.a["href"] + '"' ')')
+	dates = soup.find_all("td", {"class": "result_col_releaseDate result_type_date"})
+	for ele in dates:
+		releasedates.append(ele.div.get_text())
+	dates2 = soup.find_all("td", {"class": "result_col_deadline result_type_date"})
+	for ele in dates2:
+		deadlinedates.append(ele.div.get_text())
+
+pd.DataFrame(list(zip(description, hyperlinks, releasedates, deadlinedates))).to_csv('Ausschreibungen.csv', index=False)
+print("Suchergebnisse in 'Ausschreibungen.csv' geschrieben")
