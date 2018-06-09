88923ddb61a22ba3cba12097b0bc901bd0535240
Roman <romhedoo@gmail.com>
2018-06-09 16:57:56 +0200

    updated version

--- E-Vergage_Scraper.py
+++ E-Vergage_Scraper.py
@@ -1,17 +1,16 @@
 from bs4 import BeautifulSoup as BS
 import requests 
 import pandas as pd
-import os
 
-keywords = ['Umfrage', 'Studie', 'Befragung']
+keywords = ['Umfrage', 'Studie', 'Befragung'] #enter here your keywords
 prefix = 'https://www.evergabe-online.de/search.html?searchString='
-ending = '&publishDateRange=ALL'
+suffix = '&publishDateRange=ALL'
 prefix2 = "https://www.evergabe-online.de"
 
 description = []
-scraperlinks = []
-releasedates = []
-deadlinedates = []
+scraper_links = []
+release_dates = []
+deadline_dates = []
 hyperlinks = []
 i=0
 
@@ -19,19 +18,19 @@ print("Suche auf ",prefix2, "nach Begriff:")
 for keyword in keywords:
 	i=i+1
 	print(" ", i, ": ", keyword, sep="")
-	text = requests.get(prefix + keyword + ending).text
+	text = requests.get(prefix + keyword + suffix).text
 	soup = BS(text, "lxml")
 	descr_scraperlinks = soup.find_all("tr", {"class": ["odd", "even"]})
 	for element in descr_scraperlinks:
 		description.append(element.a.get_text()) 
-		scraperlinks.append(prefix2 + element.a["href"])
+		scraper_links.append(prefix2 + element.a["href"])
 		hyperlinks.append('=hyperlink(' '"' + prefix2 + element.a["href"] + '"' ')')
 	dates = soup.find_all("td", {"class": "result_col_releaseDate result_type_date"})
 	for ele in dates:
-		releasedates.append(ele.div.get_text())
+		release_dates.append(ele.div.get_text())
 	dates2 = soup.find_all("td", {"class": "result_col_deadline result_type_date"})
 	for ele in dates2:
-		deadlinedates.append(ele.div.get_text())
+		deadline_dates.append(ele.div.get_text())
 
-pd.DataFrame(list(zip(description, hyperlinks, releasedates, deadlinedates))).to_csv('Ausschreibungen.csv', index=False)
+pd.DataFrame(list(zip(description, hyperlinks, release_dates, deadline_dates))).to_csv('Ausschreibungen.csv', index=False)
 print("Suchergebnisse in 'Ausschreibungen.csv' geschrieben")
